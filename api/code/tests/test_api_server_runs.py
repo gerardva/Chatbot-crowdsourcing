1bb97c9633fcd341723c5b33444904ad00007d5d
Kilian Grashoff <kiliangrashoff@gmail.com>
2017-03-23 17:37:16 +0100

    Update API tests to run as separate test functions
    Integration tests are split into subtests, since they need to be
    run in a specific order.
    
    Also use python test discovery instead of nose, since it shows
    printed output and detects all tests (while nose for some reason
    skips the test_api_server_runs.py test).

--- api/code/tests/test_api_server_runs.py
+++ api/code/tests/test_api_server_runs.py
@@ -1,14 +1,12 @@
-import unittest
+from unittest import TestCase
 from urllib import request
 
-from api.code.settings import settings
 
-
-class ApiServerRunningTestCase(unittest.TestCase):
-    def runTest(self):
-        port = settings['ports']['waitress']
-        if port is None:
-            port = '5000'  # try default
+class ServerTest(TestCase):
+    def test_server_setup(self):
+        # port = settings['ports']['waitress']
+        # if port is None:
+        port = '5000'  # try default
 
         result = request.urlopen('http://127.0.0.1:' + port + '/quote').read()
-        self.assertIsInstance(result, str, "api server might be off")
+        self.assertIsInstance(result, bytes, "api server might be off")
--- api/code/tests/test_post_to_api.py
+++ api/code/tests/test_post_to_api.py
@@ -1,38 +1,59 @@
-import requests
 import json
+from unittest import TestCase
 
-r = requests.get('http://localhost:5000/worker/createNewUser')
-
-print(r.text)
-r_as_json = json.loads(r.text)
-userId = r_as_json['userId']
-
-r = requests.post('http://localhost:5000/requester/inputTask', data=json.dumps({
-    'userId': 2,
-    'data': [
-        {'pictureUrl': 'someUrl'}
-    ],
-    'questionRows': {
-        'q1': 'what location',
-        'q2': 'other q'
-    }
-}))
-
-print(r.text)
-
-r = requests.get('http://localhost:5000/worker/getRandomJob')
-
-print(r.text)
-r_as_json = json.loads(r.text)
-contentId = r_as_json['contentId']
-questionId = r_as_json['questions'][0]['questionId']
+import requests
 
 
-r = requests.post('http://localhost:5000/worker/submitAnswer', data=json.dumps({
-    'userId': userId,
-    'contentId': contentId,
-    'questionId': questionId,
-    'answer': 'this is truly a legit answer'
-}))
+class ApiIntegrationTest(TestCase):
+    userId = -1
+    contentId = -1
+    questionId = -1
 
-print(r.text)
+    def test_api(self):
+        with self.subTest(0):
+            self.create_new_user()
+        with self.subTest(1):
+            self.post_task()
+        with self.subTest(2):
+            self.get_task()
+        with self.subTest(3):
+            self.post_answer()
+
+    def create_new_user(self):
+        r = requests.get('http://localhost:5000/worker/createNewUser')
+
+        print(r.text)
+        r_as_json = json.loads(r.text)
+        self.userId = r_as_json['userId']
+
+    def post_task(self):
+        r = requests.post('http://localhost:5000/requester/inputTask', data=json.dumps({
+            'userId': self.userId,
+            'data': [
+                {'pictureUrl': 'someUrl'}
+            ],
+            'questionRows': {
+                'q1': 'what location',
+                'q2': 'other q'
+            }
+        }))
+
+        print(r.text)
+
+    def get_task(self):
+        r = requests.get('http://localhost:5000/worker/getRandomJob')
+
+        print(r.text)
+        r_as_json = json.loads(r.text)
+        self.contentId = r_as_json['contentId']
+        self.questionId = r_as_json['questions'][0]['questionId']
+
+    def post_answer(self):
+        r = requests.post('http://localhost:5000/worker/submitAnswer', data=json.dumps({
+            'userId': self.userId,
+            'contentId': self.contentId,
+            'questionId': self.questionId,
+            'answer': 'this is truly a legit answer'
+        }))
+
+        print(r.text)
